3fb389e0d817073c16b7742878bc13824c8bb5dd
Darkian98 <40228167+Darkian98@users.noreply.github.com>
2025-08-28 16:05:36 +0200

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SyncModule } from './sync/sync.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SyncModule],
+  imports: [SyncModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT', 8000);
+  const corsOrigins = configService.get<string>('CORS_ORIGIN', '*');
+  const originList = corsOrigins.split(',').map((o) => o.trim());
 
   app.enableCors({
-    origin: 'http://localhost:3000', // o '*' para todos
+    origin: originList, // o '*' para todos
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
   });
 
-  
+
   const config = new DocumentBuilder()
     .setTitle('VideoSync api')
     .setDescription('The cats API description')
@@ -21,6 +26,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(PORT);
 }
 bootstrap();
